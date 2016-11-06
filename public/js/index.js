b8b37f7fa5dd2dcb83a39bb42b92a88527b05a2f
Jonathan Rothenberg <jprothen@ucsd.edu>
2016-11-06 01:30:03 -0700

    more work on index page

--- public/js/index.js
+++ public/js/index.js
@@ -15,19 +15,19 @@ function initializePage() {
     } }
   ];
    var side_enter = [
-    {selector: '#row1', offset: 50, callback: function(el) {
+    {selector: '#row1', offset: 20, callback: function(el) {
     	$('#card_school').show();
     	$('#card_school').addClass('animated slideInLeft');
     } }
   ];
    var side_enter1 = [
-    {selector: '#row2', offset: 20, callback: function(el) {
+    {selector: '#row2', offset: 10, callback: function(el) {
     	$('#card_home').show();
     	$('#card_home').addClass('animated slideInLeft');
     } }
   ];
    var side_enter2 = [
-    {selector: '#row3', offset: 20, callback: function(el) {
+    {selector: '#row3', offset: 15, callback: function(el) {
     	$('#card_about').show();
     	$('#card_about').addClass('animated slideInLeft');
     } }
