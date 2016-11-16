f7113ab0b9dbd2bb4724be2adc4603ecf1958a89
Reese Wahlin <wahliner@gmail.com>
2016-11-15 20:22:40 -0800

    made homepage render things based on whats closed to you

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,18 +1,66 @@
 'use strict';
 var user;
+var school, airport, closest, close;
+var whatIsClosest;
 var file_path = "From public/js/homepage.js: ";
+var airport_details = {
+        "link": "/newairport",
+        "image": "airplane.png",
+        "intro": "INFORMATION FOR",
+        "name": "AIRPORT"
+    }, school_details = {
+        "link": "/newschool",
+        "image": "school.png",
+        "intro": "INFORMATION FOR",
+        "name": "SCHOOL"
+    }
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
     $.get('/currentuser', function (currentUser) {
         user = currentUser;
-        console.log(file_path+"User after get request: "+user.first_name);
+        if (!user) {alert("Please return to the Login screen and login.")}
+        if (user.closest.name.includes("Airport")) {
+          airport = closest = user.closest;
+          school = close = user.close;     
+          whatIsClosest = "airport";     
+        } else {
+          airport = close = user.close;
+          school = closest = user.closest;
+          whatIsClosest = "school";
+        }
+        console.log(file_path+"User after get request: "+airport_details['link']);
         populatePage();
     });
 })
 
+//order the cards based on whats closest
 function populatePage() {
-    $(".airport_name").text(user.closest.name)
+    if (whatIsClosest == "school") {
+        //card1
+        $(".card1_link").attr("href", school_details['link']);
+        $(".card1_image").attr("src", "/images/"+school_details['image']);
+        $(".card1_intro").text(school_details['intro']);
+        $(".card1_name").text(school_details['name']);
+
+        //card2
+        $(".card2_link").attr("href", airport_details['link']);
+        $(".card2_image").attr("src", "/images/"+airport_details['image']);
+        $(".card2_intro").text(airport_details['intro']);
+        $(".card2_name").text(airport_details['name']);
+    } else {
+        //card1
+        $(".card2_link").attr("href", school_details['link']);
+        $(".card2_image").attr("src", "/images/"+school_details['image']);
+        $(".card2_intro").text(school_details['intro']);
+        $(".card2_name").text(school_details['name']);
+
+        //card2
+        $(".card1_link").attr("href", airport_details['link']);
+        $(".card1_image").attr("src", "/images/"+airport_details['image']);
+        $(".card1_intro").text(airport_details['intro']);
+        $(".card1_name").text(airport_details['name']);    }
 }
 
 /*
